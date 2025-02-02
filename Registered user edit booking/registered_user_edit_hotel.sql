63312d571537a615338787a81130e4004cff43db
Tarmeesee Daoh <tarmizee.smp01@gmail.com>
2025-02-02 14:31:34 +0700

    add "registered_user_edit_hotel"

---
+++ Registered user edit booking/registered_user_edit_hotel.sql
@@ -0,0 +1,23 @@
+create or replace procedure registered_user_edit_hotel(
+    change_booking_id int,
+    change_hotel varchar
+)
+language plpgsql
+as
+$$
+declare
+    to_hotel varchar := change_hotel;
+begin 
+    if exists (
+        select 1
+        from hotel
+        where change_hotel = hotel_name -- doesn't have hotel_name in hotel table
+    ) then
+        update booking_transaction
+        set preferred_hotel = change_hotel
+        where change_booking_id = booking_id;
+    else
+        raise notice '% does not exist', to_hotel;
+    end if;    
+end
+$$
