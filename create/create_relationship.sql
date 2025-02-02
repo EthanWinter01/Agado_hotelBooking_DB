570252438399c1bfcc4a8468ac3bd5ddcf80661b
Ethan Winter <thanpisit1403@gmail.com>
2025-02-02 17:28:14 +0700

    format create folder

---
+++ create/create_relationship.sql
@@ -0,0 +1,40 @@
+drop table if exists booking_transaction;
+drop table if exists view_hotel_info;
+drop table if exists edit_overall_info;
+drop table if exists manages_bookings;
+
+--log contain all booking
+create table booking_transaction(
+    booking_id int primary key,
+    booking_date timestamp not null,
+    duration int,
+    preferred_hotel varchar(255),
+    check_in_date date not null,
+    check_out_date date not null,
+    user_id int,
+    foreign key(user_id) references usert(user_id)
+);
+
+--log for user view each hotel, for rating and popularity
+create table view_hotel_info(
+    hotel_id int,
+    user_id int,
+    foreign key(hotel_id) references hotel(hotel_id),
+    foreign key(user_id) references usert(user_id)
+);
+
+--log for admin to edit hotel details
+create table edit_overall_info(
+    hotel_id int,
+    user_id int,
+    foreign key(hotel_id) references hotel(hotel_id),
+    foreign key(user_id) references usert(user_id)
+);
+
+--log for admin edit booking details
+create table manages_booking(
+    user_id int,
+    booking_id int,
+    foreign key(booking_id) references booking_transaction(booking_id),
+    foreign key(user_id) references usert(user_id)
+);
