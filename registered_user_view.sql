68581c4f4c76b5fa827c79c17316de870acd409a
Tarmeesee Daoh <tarmizee.smp01@gmail.com>
2025-02-02 13:52:39 +0700

    change file name "user_view" -> "registered_user_view"

---
+++ registered_user_view.sql
@@ -0,0 +1,18 @@
+create or replace function registered_user_view(view_id int)
+    return table(
+        hotel varchar,
+        check_in_date date,
+        check_out_date date,
+        booking_date date
+    ) 
+language plpgsql
+as
+$$
+begin
+    return query
+    select B.preferred_hotel, B.check_in_date, B.check_out_date, B.booking_date
+    from booking_transaction B
+    where B.user_id = view_id
+    order by B.booking_date desc;
+end;
+$$    
