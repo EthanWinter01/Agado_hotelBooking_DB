336754c3a35c0ad2af5fb1e3494762fad772aedf
Tarmeesee Daoh <tarmizee.smp01@gmail.com>
2025-02-02 22:08:13 +0700

    edit " registered_user_edit_date\

--- Registered user edit booking/registered_user_edit_date.sql
+++ Registered user edit booking/registered_user_edit_date.sql
@@ -7,15 +7,17 @@ language plpgsql
 as
 $$
 begin
-    if change_check_out - change_check_in <= 3 then
+    if change_check_in > change_check_out then
+        raise notice 'check in time must come before check out time';
+    elsif change_check_out - change_check_in <= 3 then
         update booking_transaction
 		set check_in_date = change_check_in, 
 		check_out_date = change_check_out,
 		duration = change_check_out - change_check_in
 		where booking_id = change_booking_id;
-    else then
+    else
         raise notice 'It more than 3 night';
-    end if;     
-    commit;
+    end if;
+	commit;
 end;
 $$    
