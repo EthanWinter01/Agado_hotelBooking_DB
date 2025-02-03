72c17245872120a980521e1e819f93ed9774814f
Ethan Winter <thanpisit1403@gmail.com>
2025-02-03 16:44:48 +0700

    ...

--- sample_data/user1.sql
+++ sample_data/user1.sql
@@ -87,21 +87,3 @@ call insert_user(
     'mobile', 0, 'Ethan Walker', 'password345', '5678901234', 'ethan.walker@email.com', 
     current_timestamp, '1997-07-15', 'registerred_user'
 );
-
-CALL insert_user('iOS', 113, 'Sittipong Wongsakul', 'password123', '1090123456', 'sittipong.wongsakul@example.com', '2024-02-25 10:00:00', '1987-09-11', 'admin');
-CALL insert_user('Android', 114, 'Pimchanok Jirawat', 'password456', '1101234567', 'pimchanok.jirawat@example.com', '2024-02-26 11:00:00', '1990-02-14', 'admin');
-CALL insert_user('Windows', 115, 'Chanin Sutharak', 'password789', '1112345678', 'chanin.sutharak@example.com', '2024-02-27 12:00:00', '1992-12-22', 'admin');
-
-CALL insert_user('iOS', NULL, 'Sudarat Meesang', 'regpass123', '1123456789', 'sudarat.meesang@example.com', '2024-02-28 13:00:00', '1996-10-30', 'registerred_user');
-CALL insert_user('Android', NULL, 'Sakchai Phongsak', 'regpass456', '1134567890', 'sakchai.phongsak@example.com', '2024-03-01 14:00:00', '1994-04-17', 'registerred_user');
-CALL insert_user('Windows', NULL, 'Thanyarat Rattanasak', 'regpass789', '1145678901', 'thanyarat.rattanasak@example.com', '2024-03-02 15:00:00', '1995-03-03', 'registerred_user');
-
-CALL insert_user('iOS', NULL, 'Apichart Tanthanuch', 'regpass123', '1156789012', 'apichart.tanthanuch@example.com', '2024-03-03 16:00:00', '1991-07-25', 'registerred_user');
-CALL insert_user('Android', NULL, 'Kwanjai Chuchai', 'regpass456', '1167890123', 'kwanjai.chuchai@example.com', '2024-03-04 17:00:00', '1993-01-19', 'registerred_user');
-CALL insert_user('Windows', NULL, 'Nattanon Manachai', 'regpass789', '1178901234', 'nattanon.manachai@example.com', '2024-03-05 18:00:00', '1990-11-10', 'registerred_user');
-
-CALL insert_user('iOS', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'unregisterred_user');
-CALL insert_user('Android', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'unregisterred_user');
-CALL insert_user('Windows', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'unregisterred_user');
-
-
