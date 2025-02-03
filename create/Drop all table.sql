5d9acaf79ad0cb73709672d3023008af16f0d89b
Tarmeesee Daoh <tarmizee.smp01@gmail.com>
2025-02-03 14:54:34 +0700

    add "Drob all table"

---
+++ create/Drop all table.sql
@@ -0,0 +1,17 @@
+-- drop all table at once
+
+drop table if exists booking_transaction cascade;
+drop table if exists view_hotel_info cascade;
+drop table if exists edit_overall_info cascade;
+drop table if exists manages_booking cascade;
+
+drop table if exists admin cascade;
+drop table if exists unregisterred_user cascade;
+drop table if exists registerred_user cascade;
+drop table if exists usert cascade;
+
+drop table if exists hotel cascade;
+drop table if exists hotel_facilities cascade;
+drop table if exists phonenumber cascade;
+drop table if exists room cascade;
+drop table if exists room_facilities cascade;
