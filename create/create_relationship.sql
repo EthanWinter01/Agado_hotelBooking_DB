790099393cef1df0d0740ea3b5bde55c31efdced
Ethan Winter <thanpisit1403@gmail.com>
2025-02-03 13:58:54 +0700

    fix relation

--- create/create_relationship.sql
+++ create/create_relationship.sql
@@ -1,7 +1,7 @@
-drop table if exists booking_transaction;
-drop table if exists view_hotel_info;
-drop table if exists edit_overall_info;
-drop table if exists manages_bookings;
+drop table if exists booking_transaction cascade;
+drop table if exists view_hotel_info cascade;
+drop table if exists edit_overall_info cascade;
+drop table if exists manages_bookings cascade;
 
 --log contain all booking
 create table booking_transaction(
@@ -20,24 +20,24 @@ create table booking_transaction(
 
 --log for user view each hotel, for rating and popularity
 create table view_hotel_info(
-    hotel_id int primary key,
-    user_id int primary key,
+    hotel_id int,
+    user_id int,
     foreign key(hotel_id) references hotel(hotel_id),
     foreign key(user_id) references usert(user_id)
 );
 
 --log for admin to edit hotel details
 create table edit_overall_info(
-    hotel_id int primary key,
-    user_id int primary key,
+    hotel_id int,
+    user_id int,
     foreign key(hotel_id) references hotel(hotel_id),
     foreign key(user_id) references usert(user_id)
 );
 
 --log for admin edit booking details
 create table manages_booking(
-    user_id int primary key,
-    booking_id int primary key,
+    user_id int,
+    booking_id int,
     foreign key(booking_id) references booking_transaction(booking_id),
     foreign key(user_id) references usert(user_id)
 );
