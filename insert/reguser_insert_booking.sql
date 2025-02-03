0c10ab7937badce7c38929d3f7fb75f02983ee47
Woraphat Jittichanon <guid.4012@gmail.com>
2025-02-03 19:11:16 +0700

    edit reguser add booking

--- insert/reguser_insert_booking.sql
+++ insert/reguser_insert_booking.sql
@@ -10,18 +10,39 @@ AS $$
 DECLARE
 new_booking_id int;
 duration int;
+admin_id int;
 BEGIN
 	new_booking_id = util_gen_booking_id();
 	duration = check_out_date - check_in_date;
+
+	-- If more than 3 night
 	IF duration > 3 
 	THEN RAISE EXCEPTION 'Book more than 3 night. Your duration : %', duration;
+
+	-- If room not available
 	ELSEIF (SELECT R.status FROM room R WHERE R.room_id = book_room_id AND R.hotel_id = book_hotel_id) = FALSE
 	THEN RAISE EXCEPTION 'Room ID : % in Hotel ID : % not available',book_room_id,book_hotel_id;
+
+	
 	ELSE
+	-- Add booking_transaction
 	INSERT INTO booking_transaction (booking_id,booking_date,duration,check_in_date,check_out_date,user_id,hotel_id,room_id)
 	VALUES (new_booking_id,now(),duration,check_in_date,check_out_date,book_user_id,book_hotel_id,book_room_id);
 
+	-- Update room status to false
 	UPDATE room SET status = FALSE WHERE room_id = book_room_id AND hotel_id = book_hotel_id;
+
+	-- Add own booking user manages_booking
+	INSERT INTO manages_booking (user_id,booking_id)
+	VALUES (book_user_id,new_booking_id);
+
+	-- Add admin manages_booking
+	FOR admin_id in (SELECT user_id FROM admin)
+	LOOP 
+	INSERT INTO manages_booking (user_id,booking_id)
+	VALUES (admin_id,new_booking_id);
+	END LOOP;
+	
 	END IF;
 	
 END;
