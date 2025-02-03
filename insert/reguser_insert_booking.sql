c52a25a3065c02d2efd230411626637115b0b30f
Woraphat Jittichanon <guid.4012@gmail.com>
2025-02-03 18:55:13 +0700

    edit reguser add booking

--- insert/reguser_insert_booking.sql
+++ insert/reguser_insert_booking.sql
@@ -1,10 +1,28 @@
-create or replace function book(
-    in    
+CREATE OR REPLACE PROCEDURE book(
+check_in_date date,
+check_out_date date,
+book_user_id int,
+book_hotel_id int,
+book_room_id int
 ) 
-language plpgsql
-as $$
-declare
+LANGUAGE plpgsql
+AS $$
+DECLARE
+new_booking_id int;
+duration int;
+BEGIN
+	new_booking_id = util_gen_booking_id();
+	duration = check_out_date - check_in_date;
+	IF duration > 3 
+	THEN RAISE EXCEPTION 'Book more than 3 night. Your duration : %', duration;
+	ELSEIF (SELECT R.status FROM room R WHERE R.room_id = book_room_id AND R.hotel_id = book_hotel_id) = FALSE
+	THEN RAISE EXCEPTION 'Room ID : % in Hotel ID : % not available',book_room_id,book_hotel_id;
+	ELSE
+	INSERT INTO booking_transaction (booking_id,booking_date,duration,check_in_date,check_out_date,user_id,hotel_id,room_id)
+	VALUES (new_booking_id,now(),duration,check_in_date,check_out_date,book_user_id,book_hotel_id,book_room_id);
 
-begin
-
-end; $$
+	UPDATE room SET status = FALSE WHERE room_id = book_room_id AND hotel_id = book_hotel_id;
+	END IF;
+	
+END;
+$$
