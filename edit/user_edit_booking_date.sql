29987a462808f9fd5a0a18ef563ed9dd0274411f
Tarmeesee Daoh <tarmizee.smp01@gmail.com>
2025-02-03 20:42:59 +0700

    edit user_edit_booking_date

--- edit/user_edit_booking_date.sql
+++ edit/user_edit_booking_date.sql
@@ -1,7 +1,8 @@
 create or replace procedure user_edit_date(
     change_booking_id int,
     change_check_in date,
-    change_check_out date
+    change_check_out date,
+    user_id_edit int
 )
 language plpgsql
 as
@@ -9,12 +10,19 @@ $$
 begin
     if change_check_in > change_check_out then
         raise notice 'check in time must come before check out time';
+        
     elsif change_check_out - change_check_in <= 3 then
         update booking_transaction
 		set check_in_date = change_check_in, 
 		check_out_date = change_check_out,
 		duration = change_check_out - change_check_in
-		where booking_id = change_booking_id;
+		where booking_id = change_booking_id
+
+        if user_id_edit >= 900000000 then
+            insert into manages_booking(user_id, booking_id, edit_timestamp)
+            VALUES (user_id_edit, change_booking_id, cast(now() as timestamp));
+        end if;
+
     else
         raise notice 'It more than 3 night';
     end if;
