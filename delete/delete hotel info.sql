eb8724a57b99cd15b218b8f2f2a7c4edb29cd59d
Warapong Thongkhundam <nat.n.dragon@gmail.com>
2025-02-03 13:59:24 +0700

    Add deletehotelinfo

---
+++ delete/delete hotel info.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE PROCEDURE Delete_Hotel_info (
+    admin_id INT,
+    p_hotel_id INT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE is_admin INT; exists_hotel INT;
+BEGIN
+    -- Check if the user is an admin
+    SELECT COUNT(*) INTO is_admin FROM admin WHERE user_id = admin_id;
+    IF is_admin = 0 THEN
+        RAISE EXCEPTION 'Permission denied: Only admins can delete hotels';
+    END IF;
+    
+    -- Check if hotel exists
+    SELECT COUNT(*) INTO exists_hotel FROM hotel WHERE hotel_id = p_hotel_id;
+    IF exists_hotel = 0 THEN
+        RAISE EXCEPTION 'Error: Hotel does not exist';
+    END IF;
+    
+    DELETE FROM phonenumber WHERE hotel_id = p_hotel_id;
+    DELETE FROM hotel_facilities WHERE hotel_id = p_hotel_id;
+    DELETE FROM room WHERE hotel_id = p_hotel_id;
+    DELETE FROM room_facilities WHERE hotel_id = p_hotel_id;
+    DELETE FROM hotel WHERE hotel_id = p_hotel_id;
+    
+    RAISE NOTICE 'Hotel deleted successfully';
+END; $$
