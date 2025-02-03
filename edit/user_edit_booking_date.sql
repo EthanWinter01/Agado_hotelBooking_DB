d64baa4cd8c8b817754d0b7f7625ae32a66effdd
Tarmeesee Daoh <tarmizee.smp01@gmail.com>
2025-02-03 22:57:43 +0700

    testing edit booking

--- edit/user_edit_booking_date.sql
+++ edit/user_edit_booking_date.sql
@@ -8,24 +8,33 @@ LANGUAGE PLPGSQL
 AS
 $$
 BEGIN
-    IF change_check_in > change_check_out THEN
-        RAISE EXCEPTION 'check in time must come before check out time';
+    IF NOT EXISTS (
+        SELECT 1
+        FROM admin a, registerred_user r 
+        WHERE a.user_id = user_id_edit OR r.user_id = user_id_edit
+    ) THEN
+        RAISE EXCEPTION 'there are no user id %', user_id_edit;
+    
+    ELSE 
+        IF change_check_in > change_check_out THEN
+            RAISE EXCEPTION 'check in time must come before check out time';
 
-    ELSIF change_check_out - change_check_in <= 3 THEN
-        UPDATE booking_transaction
-		SET check_in_date = change_check_in, 
-		check_out_date = change_check_out,
-		duration = change_check_out - change_check_in
-		WHERE booking_id = change_booking_id;
+        ELSIF change_check_out - change_check_in <= 3 THEN
+            UPDATE booking_transaction
+            SET check_in_date = change_check_in, 
+            check_out_date = change_check_out,
+            duration = change_check_out - change_check_in
+            WHERE booking_id = change_booking_id;
 
-        INSERT INTO manages_booking(user_id, booking_id, edit_timestamp)
-        VALUES (user_id_edit, change_booking_id, cast(NOW() AS TIMESTAMP));
+            INSERT INTO manages_booking(user_id, booking_id, edit_timestamp)
+            VALUES (user_id_edit, change_booking_id, cast(NOW() AS TIMESTAMP));
 
-        RAISE NOTICE 'change date successfully';
+            RAISE NOTICE 'change date successfully';
 
-    ELSE
-        RAISE EXCEPTION 'It more than 3 night';
-    END IF;
+        ELSE
+            RAISE EXCEPTION 'It more than 3 night';
+        END IF;
+    END IF;    
 	COMMIT;
 END;
 $$    
--- edit/user_edit_booking_hotel.sql
+++ edit/user_edit_booking_hotel.sql
@@ -9,31 +9,41 @@ $$
 DECLARE
     new_room_id INT;
 BEGIN 
-    SELECT min(room_id) 
-    INTO new_room_id
-    FROM hotel h
-    JOIN room r ON h.hotel_id = r.hotel_id
-    WHERE h.hotel_id = change_hotel_id 
-    AND r.status = true;
+    IF NOT EXISTS (
+        SELECT 1
+        FROM ADMIN a, registerred_user r 
+        WHERE a.user_id = user_id_edit OR r.user_id = user_id_edit
+    ) THEN
+        RAISE EXCEPTION 'there are no user id %', user_id_edit;
+    
+    ELSE 
 
-    IF new_room_id IS NOT NULL THEN
-        UPDATE booking_transaction
-        SET hotel_id = change_hotel_id,
-        room_id = new_room_id
-        WHERE change_booking_id = booking_id;
+        SELECT min(room_id) 
+        INTO new_room_id
+        FROM hotel h
+        JOIN room r ON h.hotel_id = r.hotel_id
+        WHERE h.hotel_id = change_hotel_id 
+        AND r.status = true;
 
-        UPDATE room
-        SET STATUS = false
-        WHERE room_id = new_room_id AND hotel_id = change_hotel_id;
+        IF new_room_id IS NOT NULL THEN
+            UPDATE booking_transaction
+            SET hotel_id = change_hotel_id,
+            room_id = new_room_id
+            WHERE change_booking_id = booking_id;
 
-        INSERT INTO manages_booking(user_id, booking_id, edit_timestamp)
-        VALUES (user_id_edit, change_booking_id, cast(NOW() AS TIMESTAMP));
+            UPDATE room
+            SET STATUS = false
+            WHERE room_id = new_room_id AND hotel_id = change_hotel_id;
 
-        RAISE NOTICE 'Hotel updated successfully.';
+            INSERT INTO manages_booking(user_id, booking_id, edit_timestamp)
+            VALUES (user_id_edit, change_booking_id, cast(NOW() AS TIMESTAMP));
 
-    ELSE
-        RAISE EXCEPTION 'the hotel does not exist, or you prefer hotel does not have available room';
-    END IF;    
+            RAISE NOTICE 'Hotel updated successfully.';
+
+        ELSE
+            RAISE EXCEPTION 'the hotel does not exist, or you prefer hotel does not have available room';
+        END IF; 
+    END IF;       
     COMMIT;
 END
 $$
