91a2f365aa5a0d66baf0f7c5b732e7c1bff1ff22
Tarmeesee Daoh <tarmizee.smp01@gmail.com>
2025-02-04 14:28:29 +0700

    add view hotel info test

--- view/view_hotel_info.sql
+++ view/view_hotel_info.sql
@@ -8,18 +8,39 @@ RETURNS TABLE(
     hotel_location VARCHAR,
     hotel_phonenumber VARCHAR, 
     check_in_time TIME,
-    check_out_time TIME
+    check_out_time TIME,
+    wifi VARCHAR,
+    pool VARCHAR,
+    valet_parking VARCHAR,
+    available_room INT -- how to also return this
 )
 LANGUAGE plpgsql
 AS
 $$
+DECLARE
+    available INT;
 BEGIN
-    INSERT INTO edit_overall_info(hotel_id, user_id, edit_timestamp)
-    VALUES (view_hotel_id, view_user_id, CAST(NOW() AS TIMESTAMP));
+    IF EXISTS (
+        SELECT 1
+        FROM hotel h
+        WHERE h.hotel_id = view_hotel_id
+    ) THEN 
+        INSERT INTO view_hotel_info(hotel_id, user_id, view_timestamp)
+        VALUES (view_hotel_id, view_user_id, CAST(NOW() AS TIMESTAMP));
 
-    RETURN QUERY
-    SELECT h.hotel_id, h.map_url, h.hotel_location, h.hotel_phonenumber, h.check_in_time, h.check_out_time
-    FROM hotel h
-    WHERE h.hotel_id = view_hotel_id;
+        SELECT COUNT(CASE WHEN r.status = true THEN 1 END)
+        INTO available
+        FROM room r 
+        WHERE r.hotel_id = view_hotel_id;
+
+        RETURN QUERY
+        SELECT h.hotel_id, h.map_url, h.hotel_location, h.hotel_phonenumber, h.check_in_time, h.check_out_time, f.wifi, f.pool, f.valet_parking, available
+        FROM hotel h
+        LEFT JOIN hotel_facilities f ON h.hotel_id = f.hotel_id
+        WHERE h.hotel_id = view_hotel_id;
+    
+    ELSE
+        RAISE NOTICE 'hotel id % does not exists', view_hotel_id;
+    END IF;    
 END;
 $$
