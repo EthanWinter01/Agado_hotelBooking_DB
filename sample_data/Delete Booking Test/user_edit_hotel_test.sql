53779c9f3da6444866f57a025aea5763865ca8f8
Tarmeesee Daoh <tarmizee.smp01@gmail.com>
2025-02-04 10:42:24 +0700

    edit test sample

--- sample_data/Delete Booking Test/user_edit_hotel_test.sql
+++ sample_data/Delete Booking Test/user_edit_hotel_test.sql
@@ -6,4 +6,4 @@ call user_edit_hotel(100000005,	10001,	900000005);
 call user_edit_hotel(100000006,	10005,	500000006);
 call user_edit_hotel(100000006,	10001,	500000009);
 call user_edit_hotel(100000007,	10001,	900000001);
-call user_edit_hotel(100000007,	11111,	900000005);
+call user_edit_hotel(100000007,	11111,	900000005); --the hotel does not exist, or you prefer hotel does not have available room
