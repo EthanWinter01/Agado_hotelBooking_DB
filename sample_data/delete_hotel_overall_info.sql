ccfd3db34bce77a9e1c7411e9d8e1d895d81c5bc
Hattakorn Hattakarn <hattakorn8895@gmail.com>
2025-02-04 10:50:42 +0700

    edit sample delete hotel info

--- sample_data/delete_hotel_overall_info.sql
+++ sample_data/delete_hotel_overall_info.sql
@@ -1,10 +1,5 @@
 CALL delete_hotel_overall_info(900000001, 10000);
 CALL delete_hotel_overall_info(900000002, 10001);
-CALL delete_hotel_overall_info(900000003, 10002);
 CALL delete_hotel_overall_info(900000004, 10003);
 CALL delete_hotel_overall_info(900000005, 10004);
 CALL delete_hotel_overall_info(900000001, 10005);
-CALL delete_hotel_overall_info(900000002, 10006);
-CALL delete_hotel_overall_info(900000003, 10007);
-CALL delete_hotel_overall_info(900000004, 10008);
-CALL delete_hotel_overall_info(900000005, 10009);
