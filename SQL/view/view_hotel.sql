fa25676929582abfb4d4c110b8c6fd018b15d963
Ethan Winter <thanpisit1403@gmail.com>
2025-02-08 21:43:04 +0700

    delete room & delete room type fac

--- SQL/view/view_hotel.sql
+++ SQL/view/view_hotel.sql
@@ -9,9 +9,9 @@ RETURNS TABLE(
     hotel_phonenumber VARCHAR, 
     check_in_time TIME,
     check_out_time TIME,
-    wifi VARCHAR,
-    pool VARCHAR,
-    valet_parking VARCHAR,
+    -- wifi VARCHAR,
+    -- pool VARCHAR,
+    -- valet_parking VARCHAR,
     available_room INT 
 )
 LANGUAGE plpgsql
@@ -22,22 +22,29 @@ DECLARE
 BEGIN
     IF EXISTS (
         SELECT 1
-        FROM hotel h
-        WHERE h.hotel_id = view_hotel_id
+            FROM hotel h
+            WHERE h.hotel_id = view_hotel_id
     ) THEN 
-        INSERT INTO view_hotel_info(hotel_id, user_id, view_timestamp)
-        VALUES (view_hotel_id, view_user_id, CAST(NOW() AS TIMESTAMP));
+        IF user_id < 900000000 THEN --Log for common people
+            INSERT INTO hotel_log(hotel_id, user_id, action_timestamp, action_type, action_description)
+                VALUES (view_hotel_id, 
+                    view_user_id, 
+                    CAST(NOW() AS TIMESTAMP),
+                    'user_view_hotel',
+                    NULL
+                );
 
         SELECT COUNT(CASE WHEN r.status = true THEN 1 END)
-        INTO available
-        FROM room r 
-        WHERE r.hotel_id = view_hotel_id;
+            INTO available
+            FROM room r 
+            WHERE r.hotel_id = view_hotel_id;
 
         RETURN QUERY
+        
         SELECT h.hotel_id, h.map_url, h.hotel_location, h.hotel_phonenumber, h.check_in_time, h.check_out_time, f.wifi, f.pool, f.valet_parking, available
-        FROM hotel h
-        LEFT JOIN hotel_facilities f ON h.hotel_id = f.hotel_id
-        WHERE h.hotel_id = view_hotel_id;
+            FROM hotel h
+            LEFT JOIN hotel_facilities f ON h.hotel_id = f.hotel_id
+            WHERE h.hotel_id = view_hotel_id;
     
     ELSE
         RAISE NOTICE 'hotel id % does not exists', view_hotel_id;
