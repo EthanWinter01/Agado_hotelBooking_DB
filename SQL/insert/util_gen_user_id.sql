076c7bb6e6c0d9de26211787ed53f436a2d873f3
Ethan Winter <thanpisit1403@gmail.com>
2025-02-08 11:41:08 +0700

    fix: wording in insert

--- SQL/insert/util_gen_user_id.sql
+++ SQL/insert/util_gen_user_id.sql
@@ -8,14 +8,14 @@ DECLARE
     new_user_id INT;
     base_id INT;
 BEGIN 
-    CALL user_type 
+    CASE user_type 
         WHEN 'admin' THEN base_id := 900000000;
         WHEN 'registerred_user' THEN base_id := 500000000;
         WHEN 'unregisterred_user' THEN base_id := 100000000;
         ELSE 
-            raise exception 'Invalid user type: %', user_type;
+            RAISE EXCEPTION 'Invalid user type: %', user_type;
     END CASE;
-    EXECUTE FOMRAT('SELECT COALESCE(MAX(user_id) - %s, 0) + %s + 1 FROM %I', base_id, base_id, user_type)
+    EXECUTE FORMAT('SELECT COALESCE(MAX(user_id) - %s, 0) + %s + 1 FROM %I', base_id, base_id, user_type)
         INTO new_user_id;
     RETURN new_user_id;
 END; $$
