562299daa5bf82ac4debf76f6c02a186af4997d6
Ethan Winter <thanpisit1403@gmail.com>
2025-02-08 22:58:59 +0700

    fix: minor problem

--- SQL/complex_query/top_perform.sql
+++ SQL/complex_query/top_perform.sql
@@ -1,8 +1,7 @@
 SELECT 
     h.hotel_id, 
     h.hotel_location, 
-    COUNT(DISTINCT bt.booking_id) AS total_bookings,  
-    ROUND(AVG(bt.duration)) AS avg_stay_duration
+    COUNT(DISTINCT bt.booking_id) AS total_bookings, 
 FROM hotel h
 LEFT JOIN booking_transaction bt ON h.hotel_id = bt.hotel_id
 GROUP BY h.hotel_id, h.hotel_location
