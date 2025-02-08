8b81d9871fee391741c24ae60ca538d5b1fd261b
Tarmeesee Daoh <tarmizee.smp01@gmail.com>
2025-02-08 23:32:39 +0700

    create sample for update_room and room facilities

---
+++ SQL/sample_data/sample_update_room_facil.sql
@@ -0,0 +1,10 @@
+call update_room_type_facilities(10000, 'Cheap', 'Basic amenities: Twin beds, air conditioning, shared bathroom, free Wi-Fi');
+call update_room_type_facilities(10001, 'Premium', 'Luxury amenities: King-size bed, private balcony, whirlpool tub, in-room dining, 24/7 butler service');
+call update_room_type_facilities(10002, 'Premium', 'Luxury amenities: King-size bed, private balcony, whirlpool tub, in-room dining, 24/7 butler service');
+call update_room_type_facilities(10003, 'Medium', 'Comfortable amenities: Queen-size bed, private bathroom, flat-screen TV, mini-fridge, complimentary coffee/tea');
+call update_room_type_facilities(10004, 'Cheap', 'Basic amenities: Twin beds, air conditioning, shared bathroom, free Wi-Fi');
+call update_room_type_facilities(10005, 'Cheap', 'Basic amenities: Twin beds, air conditioning, shared bathroom, free Wi-Fi');
+call update_room_type_facilities(10006, 'Premium', 'Luxury amenities: King-size bed, private balcony, whirlpool tub, in-room dining, 24/7 butler service');
+call update_room_type_facilities(10007, 'Premium', 'Luxury amenities: King-size bed, private balcony, whirlpool tub, in-room dining, 24/7 butler service');
+call update_room_type_facilities(10008, 'Medium', 'Comfortable amenities: Queen-size bed, private bathroom, flat-screen TV, mini-fridge, complimentary coffee/tea');
+call update_room_type_facilities(10009, 'Cheap', 'Basic amenities: Twin beds, air conditioning, shared bathroom, free Wi-Fi');
