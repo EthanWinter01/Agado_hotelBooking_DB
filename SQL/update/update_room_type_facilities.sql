68bbbd2197e3def82774f04f94fd2237af5b12d6
Ethan Winter <thanpisit1403@gmail.com>
2025-02-08 21:23:49 +0700

    room type facilities

---
+++ SQL/update/update_room_type_facilities.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE PROCEDURE update_room_type_facilities(
+    update_hotel_id INT, 
+    update_room_type VARCHAR(64), 
+    update_room_facilities VARCHAR(256)
+)
+LANGUAGE plpgsql 
+AS $$
+BEGIN
+    UPDATE room_type_facilities
+        SET room_facilities = update_status,
+        WHERE hotel_id = update_hotel_id AND room_type = update_room_type;
+    RAISE NOTICE 'Room Facilities updated successfully';
+END; $$;
