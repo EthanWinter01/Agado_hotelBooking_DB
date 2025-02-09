7d0d60ff7b0315ccfc41ad17c6757189611c919f
Tarmeesee Daoh <tarmizee.smp01@gmail.com>
2025-02-09 22:17:55 +0700

    remove unusing file

--- SQL/sample_data/Delete Booking Test/insert hotel for test.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO hotel (hotel_id, map_url, hotel_location, check_in_time, check_out_time) VALUES
-(1, 'https://maps.example.com/hotel1', 'Bangkok, Thailand', '14:00', '12:00'),
-(2, 'https://maps.example.com/hotel2', 'Chiang Mai, Thailand', '15:00', '11:00'),
-(3, 'https://maps.example.com/hotel3', 'Phuket, Thailand', '14:30', '11:30'),
-(4, 'https://maps.example.com/hotel4', 'Pattaya, Thailand', '13:00', '12:00'),
-(5, 'https://maps.example.com/hotel5', 'Hua Hin, Thailand', '14:00', '11:00');
-
-
-
--- SQL/sample_data/delete_hotel_overall_info.sql
+++
@@ -1,5 +0,0 @@
-CALL delete_hotel_overall_info(900000001, 10000);
-CALL delete_hotel_overall_info(900000002, 10001);
-CALL delete_hotel_overall_info(900000004, 10003);
-CALL delete_hotel_overall_info(900000005, 10004);
-CALL delete_hotel_overall_info(900000001, 10005);
