230e8305e1e013d4da299ecc7d9f3da5537bf639
Tarmeesee Daoh <tarmizee.smp01@gmail.com>
2025-02-09 22:06:29 +0700

    change variable and delete status attribute in room that use in delete_booking

--- SQL/delete/delete_booking.sql
+++ SQL/delete/delete_booking.sql
@@ -1,38 +1,32 @@
 CREATE OR REPLACE PROCEDURE delete_booking(
-delete_id INT,
+delete_booking_id INT,
 deleter_id INT
 )
 LANGUAGE plpgsql
 AS $$
 BEGIN
 	IF deleter_id >= 900000000 THEN
-		UPDATE room 
-			SET status = true 
-			WHERE room_id = (SELECT room_id FROM booking_transaction WHERE booking_id = delete_id) 
-			AND hotel_id = (SELECT hotel_id FROM booking_transaction WHERE booking_id = delete_id);
-
+		
 		DELETE FROM booking_log 
-			WHERE booking_id = delete_id;
+			WHERE booking_id = delete_booking_id;
 	
 		DELETE FROM booking_transaction 
-			WHERE booking_id = delete_id;
-	ELSEIF deleter_id = (SELECT user_id FROM booking_transaction WHERE booking_id = delete_id ) THEN
-		UPDATE room 
-			SET status = true 
-			WHERE room_id = (SELECT room_id FROM booking_transaction WHERE booking_id = delete_id) 
-			AND hotel_id = (SELECT hotel_id FROM booking_transaction WHERE booking_id = delete_id);
+			WHERE booking_id = delete_booking_id;
 	
+	ELSEIF deleter_id = (SELECT user_id FROM booking_transaction WHERE booking_id = delete_booking_id ) THEN
+		
 		delete FROM booking_log
-			WHERE booking_id = delete_id;
+			WHERE booking_id = delete_booking_id;
 		
 		delete FROM booking_transaction 
-			WHERE booking_id = delete_id;
+			WHERE booking_id = delete_booking_id;
+	
 	ELSE raise exception 'Deleter try to delete other user booking';
 	END IF;
 END;$$
 
 -- CREATE OR REPLACE PROCEDURE Delete_Booking(
--- delete_id INT,
+-- delete_booking_id INT,
 -- deleter_id INT
 -- )
 -- LANGUAGE plpgsql
@@ -41,25 +35,25 @@ END;$$
 -- 	IF deleter_id >= 900000000 THEN
 -- 		UPDATE room 
 -- 			SET status = true 
--- 			WHERE room_id = (SELECT room_id FROM booking_transaction WHERE booking_id = delete_id) 
--- 			AND hotel_id = (SELECT hotel_id FROM booking_transaction WHERE booking_id = delete_id);
+-- 			WHERE room_id = (SELECT room_id FROM booking_transaction WHERE booking_id = delete_booking_id) 
+-- 			AND hotel_id = (SELECT hotel_id FROM booking_transaction WHERE booking_id = delete_booking_id);
 
 -- 		DELETE FROM manages_booking 
--- 			WHERE booking_id = delete_id;
+-- 			WHERE booking_id = delete_booking_id;
 	
 -- 		DELETE FROM booking_transaction 
--- 			WHERE booking_id = delete_id;
--- 	ELSEIF deleter_id = (SELECT user_id FROM booking_transaction WHERE booking_id = delete_id ) THEN
+-- 			WHERE booking_id = delete_booking_id;
+-- 	ELSEIF deleter_id = (SELECT user_id FROM booking_transaction WHERE booking_id = delete_booking_id ) THEN
 -- 		UPDATE room 
 -- 			SET status = true 
--- 			WHERE room_id = (SELECT room_id FROM booking_transaction WHERE booking_id = delete_id) 
--- 			AND hotel_id = (SELECT hotel_id FROM booking_transaction WHERE booking_id = delete_id);
+-- 			WHERE room_id = (SELECT room_id FROM booking_transaction WHERE booking_id = delete_booking_id) 
+-- 			AND hotel_id = (SELECT hotel_id FROM booking_transaction WHERE booking_id = delete_booking_id);
 	
 -- 		delete FROM manages_booking 
--- 			WHERE booking_id = delete_id;
+-- 			WHERE booking_id = delete_booking_id;
 		
 -- 		delete FROM booking_transaction 
--- 			WHERE booking_id = delete_id;
+-- 			WHERE booking_id = delete_booking_id;
 -- 	ELSE raise exception 'Deleter try to delete other user booking';
 -- 	END IF;
 -- END;$$
--- SQL/sample_data/sample_delete_booking.sql
+++ SQL/sample_data/sample_delete_booking.sql
@@ -3,3 +3,10 @@ CALL delete_booking(100000005, 900000002);
 CALL delete_booking(100000010, 900000003);
 CALL delete_booking(100000015, 900000004);
 CALL delete_booking(100000019, 900000005);
+
+/*
+CREATE OR REPLACE PROCEDURE delete_booking(
+delete_booking_id INT,
+deleter_id INT
+)
+*/
