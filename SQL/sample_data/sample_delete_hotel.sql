f85866348ed68654b628b9b88a8a0ae1e6123349
Tarmeesee Daoh <tarmizee.smp01@gmail.com>
2025-02-09 17:49:23 +0700

    create sample for delete hotel and room

---
+++ SQL/sample_data/sample_delete_hotel.sql
@@ -0,0 +1,12 @@
+call delete_hotel(900000009, 10001);
+call delete_hotel(900000008, 10003);
+call delete_hotel(900000007, 10005);
+call delete_hotel(900000006, 10007);
+call delete_hotel(900000005, 10009);
+
+/*
+CREATE OR REPLACE PROCEDURE delete_hotel(
+    admin_id INT,
+    p_hotel_id INT
+)
+*/
---
+++ SQL/sample_data/sample_delete_room.sql
@@ -0,0 +1,9 @@
+call delete_room(10000, 101);
+call delete_room(10000, 201);
+call delete_room(10000, 301);
+/*
+CREATE OR REPLACE PROCEDURE delete_room(
+    del_hotel_id int,
+    del_room_id int,
+)
+*/
